01bfbe796daedfdacffd5e2345777afe07208f49
Martin Neubert <martin@martin-neubert.de>
2021-01-30 18:34:14 +0100

    update deps and widescreen in home menu

--- lib/views/pages/home.dart
+++ lib/views/pages/home.dart
@@ -8,28 +8,55 @@ import 'package:kikke/views/parts/listview.dart';
 class AppHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Problems'),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: "Services"),
-              Tab(text: "Hosts"),
-            ],
-          ),
-        ),
-        body: new Center(
-          child: TabBarView(
-            children: <Widget>[
-              IcingaObjectListView(controller: getIt.get<ServiceController>()),
-              IcingaObjectListView(controller: getIt.get<HostController>()),
-            ],
-          ),
-        ),
-        drawer: DrawerMenu(),
-      ),
+    return LayoutBuilder(
+        builder: (context, constraints) {
+
+          if(constraints.maxWidth > constraints.maxHeight) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Problems"),
+              ),
+              body: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: IcingaObjectListView(controller: getIt.get<ServiceController>()),
+                    ),
+                    Expanded(
+                      child: IcingaObjectListView(controller: getIt.get<HostController>()),
+                    ),
+                  ],
+                ),
+              ),
+              drawer: DrawerMenu(),
+            );
+          }
+
+          return DefaultTabController(
+            length: 2,
+            child: new Scaffold(
+              appBar: new AppBar(
+                title: new Text('Problems'),
+                bottom: TabBar(
+                  tabs: [
+                    Tab(text: "Services"),
+                    Tab(text: "Hosts"),
+                  ],
+                ),
+              ),
+              body: new Center(
+                child: TabBarView(
+                  children: <Widget>[
+                    IcingaObjectListView(controller: getIt.get<ServiceController>()),
+                    IcingaObjectListView(controller: getIt.get<HostController>()),
+                  ],
+                ),
+              ),
+              drawer: DrawerMenu(),
+            ),
+          );
+        }
     );
   }
 }
